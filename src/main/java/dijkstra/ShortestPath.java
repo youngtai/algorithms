b46c4248d71c8b66cec742c878aa513df1a6a71e
youngtai <youngtaiahn@gmail.com>
2022-05-30 21:08:48 -0600

    Small test graph working

--- src/main/java/dijkstra/ShortestPath.java
+++ src/main/java/dijkstra/ShortestPath.java
@@ -3,10 +3,8 @@ package dijkstra;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ShortestPath {
@@ -15,10 +13,47 @@ public class ShortestPath {
 
   }
 
+  int dijkstra(Graph graph, int targetNode) {
+    final Set<Integer> visitedNodes = new HashSet<>();
+    final int startNode = 1;
+    visitedNodes.add(startNode);  // start node is 1
+
+    System.out.println(visitedNodes);
+
+//    int shortestPath = 0;
+    final Map<Integer, Integer> shortestPaths = new HashMap<>();
+    graph.graph().keySet().forEach(node -> shortestPaths.put(node, 0));
+
+    System.out.println(shortestPaths);
+
+    while (!visitedNodes.equals(graph.graph().keySet()) && !visitedNodes.contains(targetNode)) {
+      final List<Edge> candidateEdges = visitedNodes.stream()
+          .flatMap(node -> graph.edges(node).stream())
+          .filter(edge -> !visitedNodes.contains(edge.endId()))
+          .toList();
+      // compute shortest path from start node to each edge head
+//      candidateEdges.forEach(candidateEdge -> {
+//        final int headNode = candidateEdge.endId();
+//        shortestPaths.put(headNode, shortestPaths.get(headNode) + candidateEdge.length());
+//      });
+
+      final Edge lengthMinimizingEdge = candidateEdges.stream()
+          .min(Comparator.comparing(edge -> shortestPaths.get(edge.endId())))
+          .orElseThrow(NoSuchElementException::new);
+      shortestPaths.put(lengthMinimizingEdge.endId(), shortestPaths.get(lengthMinimizingEdge.endId()) + lengthMinimizingEdge.length());
+      // Update
+      visitedNodes.add(lengthMinimizingEdge.endId());
+      System.out.println(visitedNodes);
+      System.out.println(shortestPaths);
+//      shortestPath += lengthMinimizingEdge.length();
+    }
+    return shortestPaths.get(targetNode);
+  }
+
   Graph loadGraph(String filename) {
     final Map<Integer, List<Edge>> graph = new HashMap<>();
 
-    try (Stream<String> stream = Files.lines(Path.of("C:\\Users\\young\\dev\\algorithms\\src\\main\\resources\\dijkstra\\" + filename))) {
+    try (Stream<String> stream = Files.lines(Path.of("/home/youngtai/IdeaProjects/algorithms/src/main/resources/dijkstra/" + filename))) {
       stream.forEach(line -> {
         final List<String> splitLine = Arrays.stream(line.split("\t")).toList();
         final int nodeId = Integer.parseInt(splitLine.get(0));
@@ -30,7 +65,7 @@ public class ShortestPath {
                   .toArray(Integer[]::new);
               return new Edge(nodeId, edgeDataSplit[0], edgeDataSplit[1]);
             }).toList();
-        System.out.println(nodeId + ": " + edges);
+//        System.out.println(nodeId + ": " + edges);
         graph.put(nodeId, edges);
       });
     } catch (IOException e) {
--- src/test/java/dijkstra/ShortestPathTests.java
+++ src/test/java/dijkstra/ShortestPathTests.java
@@ -5,11 +5,32 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShortestPathTests {
+  private final ShortestPath sp = new ShortestPath();
+  final Graph graph1 = sp.loadGraph("test-1.txt");
 
   @Test
   void shouldLoadGraph() {
-    final ShortestPath sp = new ShortestPath();
-    final Graph graph = sp.loadGraph("test-1.txt");
-    assertEquals(4, graph.graph().size());
+    assertEquals(4, graph1.graph().size());
+
+    assertEquals(graph1.edges(1).get(0).startId(), 1);
+    assertEquals(graph1.edges(1).get(0).endId(), 2);
+    assertEquals(graph1.edges(1).get(0).length(), 1);
+
+    assertEquals(graph1.edges(1).get(1).startId(), 1);
+    assertEquals(graph1.edges(1).get(1).endId(), 3);
+    assertEquals(graph1.edges(1).get(1).length(), 3);
+
+    assertEquals(graph1.edges(2).get(0).startId(), 2);
+    assertEquals(graph1.edges(2).get(0).endId(), 1);
+    assertEquals(graph1.edges(2).get(0).length(), 1);
+
+    assertEquals(graph1.edges(2).get(1).startId(), 2);
+    assertEquals(graph1.edges(2).get(1).endId(), 4);
+    assertEquals(graph1.edges(2).get(1).length(), 5);
+  }
+
+  @Test
+  void shouldRunDijkstra_test1() {
+    assertEquals(sp.dijkstra(graph1, 4), 5);
   }
 }
