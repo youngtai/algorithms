f4fcadab0255107047739e7ae626085d7448b5dc
youngtai <youngtaiahn@gmail.com>
2022-05-31 19:35:15 -0600

    Passing code and comments

--- src/main/java/dijkstra/ShortestPath.java
+++ src/main/java/dijkstra/ShortestPath.java
@@ -4,48 +4,60 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.lang.System.out;
 
 public class ShortestPath {
 
   public static void main(String[] args) {
-
+    final ShortestPath sp = new ShortestPath();
+    final Graph graph = sp.loadGraph("dijkstra-graph.txt");
+    //Targets: 7,37,59,82,99,115,133,165,188,197
+    //Outputs: 2599,2610,2947,2052,2367,2399,2029,2442,2505,3068
+    out.println(sp.dijkstra(graph, 7));
+    out.println(sp.dijkstra(graph, 37));
+    out.println(sp.dijkstra(graph, 59));
+    out.println(sp.dijkstra(graph, 82));
+    out.println(sp.dijkstra(graph, 99));
+    out.println(sp.dijkstra(graph, 115));
+    out.println(sp.dijkstra(graph, 133));
+    out.println(sp.dijkstra(graph, 165));
+    out.println(sp.dijkstra(graph, 188));
+    out.println(sp.dijkstra(graph, 197));
   }
 
+  /**
+   * while not all nodes are visited
+   *   among all candidate edges, choose the one that minimizes the length from the candidate edge tail + the edge length
+   *   add the length minimizing edge head to the set of visited notes
+   *   set the shortest path of the length minimizing edge head as: the shortest path to its tail + its length
+   *
+   * @param graph The undirected graph stored as an adjacency list
+   * @param targetNode The node we are trying to find a shortest path to
+   * @return The length of the shortest path to the target node
+   */
   int dijkstra(Graph graph, int targetNode) {
     final Set<Integer> visitedNodes = new HashSet<>();
     final int startNode = 1;
     visitedNodes.add(startNode);  // start node is 1
-
-    System.out.println(visitedNodes);
-
-//    int shortestPath = 0;
+//    out.println("visited: " + visitedNodes);
     final Map<Integer, Integer> shortestPaths = new HashMap<>();
     graph.graph().keySet().forEach(node -> shortestPaths.put(node, 0));
-
-    System.out.println(shortestPaths);
-
+//    out.println("shortest: " + shortestPaths);
     while (!visitedNodes.equals(graph.graph().keySet()) && !visitedNodes.contains(targetNode)) {
       final List<Edge> candidateEdges = visitedNodes.stream()
           .flatMap(node -> graph.edges(node).stream())
           .filter(edge -> !visitedNodes.contains(edge.endId()))
           .toList();
-      // compute shortest path from start node to each edge head
-//      candidateEdges.forEach(candidateEdge -> {
-//        final int headNode = candidateEdge.endId();
-//        shortestPaths.put(headNode, shortestPaths.get(headNode) + candidateEdge.length());
-//      });
-
       final Edge lengthMinimizingEdge = candidateEdges.stream()
-          .min(Comparator.comparing(edge -> shortestPaths.get(edge.endId())))
+          .min(Comparator.comparing(edge -> shortestPaths.get(edge.startId()) + edge.length()))
           .orElseThrow(NoSuchElementException::new);
-      shortestPaths.put(lengthMinimizingEdge.endId(), shortestPaths.get(lengthMinimizingEdge.endId()) + lengthMinimizingEdge.length());
-      // Update
+      shortestPaths.put(lengthMinimizingEdge.endId(), shortestPaths.get(lengthMinimizingEdge.startId()) + lengthMinimizingEdge.length());
       visitedNodes.add(lengthMinimizingEdge.endId());
-      System.out.println(visitedNodes);
-      System.out.println(shortestPaths);
-//      shortestPath += lengthMinimizingEdge.length();
+//      out.println("candidates: " + candidateEdges);
+//      out.println("visited: " + visitedNodes);
+//      out.println("shortest: " + shortestPaths + "\n");
     }
     return shortestPaths.get(targetNode);
   }
--- src/test/java/dijkstra/ShortestPathTests.java
+++ src/test/java/dijkstra/ShortestPathTests.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ShortestPathTests {
   private final ShortestPath sp = new ShortestPath();
   final Graph graph1 = sp.loadGraph("test-1.txt");
+  final Graph graph2 = sp.loadGraph("test-2.txt");
 
   @Test
   void shouldLoadGraph() {
@@ -31,6 +32,29 @@ public class ShortestPathTests {
 
   @Test
   void shouldRunDijkstra_test1() {
+    assertEquals(sp.dijkstra(graph1, 1), 0);
+    assertEquals(sp.dijkstra(graph1, 2), 1);
+    assertEquals(sp.dijkstra(graph1, 3), 3);
     assertEquals(sp.dijkstra(graph1, 4), 5);
+  }
+
+  @Test
+  void shouldRunDijkstra_test2() {
+    System.out.println("Target: 1");
+    assertEquals(sp.dijkstra(graph2, 1), 0);
+    System.out.println("Target: 2");
+    assertEquals(sp.dijkstra(graph2, 2), 1);
+    System.out.println("Target: 3");
+    assertEquals(sp.dijkstra(graph2, 3), 2);
+    System.out.println("Target: 4");
+    assertEquals(sp.dijkstra(graph2, 4), 3);
+    System.out.println("Target: 5");
+    assertEquals(sp.dijkstra(graph2, 5), 4);
+    System.out.println("Target: 6");
+    assertEquals(sp.dijkstra(graph2, 6), 4);
+    System.out.println("Target: 7");
+    assertEquals(sp.dijkstra(graph2, 7), 3);
+    System.out.println("Target: 8");
+    assertEquals(sp.dijkstra(graph2, 8), 2);
   }
 }
